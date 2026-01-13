67fc1d156861b9c645619164c3654ff6199486c5
kener <kcm.smart@gmail.com>
2026-01-13 18:00:30 -0500

    Se agrega funciones de deploy en package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 
-     site: 'https://kcmsmartdev.github.io',
-  base: '/aurummetalsoneweb/',
-  output: 'static',
+
+    base: 'https://kcmsmartdev.github.io/aurummetalsoneweb/',
+    output: 'static',
 });
